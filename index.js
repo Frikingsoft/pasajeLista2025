164b2772ed38f1fd442dd0a90dcdf7b08c988c02
frikingsoft <nicolas.barone1@gmail.com>
2025-10-06 15:55:26 -0300

    agregando array menu a login

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { router } from "./config.js"
 // Get (Obtener) POST(Enviar) PUT(Actualizar) Delete(Borrar)
 // Funcion de flecha ()=>{}
-router.get("/",(req,res)=>{
     const menu = [
         {nombre:"inicio",url:"/"},
         {nombre:"login",url:"/login"},
         {nombre:"registro",url:"/registro"}
     ]
+router.get("/",(req,res)=>{
     res.render("index.hbs",{menu})
 })
 router.get("/login",(req,res)=>{
-    res.render("login.hbs")
+    res.render("login.hbs",{menu})
 })
