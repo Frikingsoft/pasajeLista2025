03c8912db186e23889a085ee81cb36e1d97315ab
frikingsoft <nicolas.barone1@gmail.com>
2025-10-06 14:55:37 -0300

    agregando login

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { router } from "./config.js"
-
+// Get (Obtener) POST(Enviar) PUT(Actualizar) Delete(Borrar)
+// Funcion de flecha ()=>{}
 router.get("/",(req,res)=>{
     res.render("index.hbs")
+})
+router.get("/login",(req,res)=>{
+    res.render("login.hbs")
 })
